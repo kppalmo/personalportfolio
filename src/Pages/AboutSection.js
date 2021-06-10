c28123299a6ad64e8d5ac56cf090498a4b6c98bf
kppalmo <kpalmo@ithaca.edu>
2021-06-10 12:19:59 -0400

    projectStart

---
+++ src/Pages/AboutSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Row, Col, Image } from 'react-bootstrap';
+import { Layout } from './Layout';
+
+const Styles = styled.div`
+
+
+`
+
+export const AboutSection = props => (
+    <Styles>
+        <Layout>
+        <h1>Hello World</h1>
+
+
+        </Layout>
+    </Styles>
+
+)
