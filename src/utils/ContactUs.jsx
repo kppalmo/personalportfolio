adac077c1e0805629e90e10e2fbf90ef3183fd61
kppalmo <kepalmo8@gmail.com>
2021-07-10 01:30:19 -0400

    linkedPage

--- src/utils/ContactUs.jsx
+++ src/utils/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Row } from 'react-bootstrap';
-
+import { Container, Row, Form, Button } from 'react-bootstrap';
+import emailjs from 'emailjs-com';
 
 const Styles = styled.div`
 .contact-container{
@@ -16,13 +16,41 @@ const Styles = styled.div`
 
 export default function ContactUs() {
 
-  return (
-    <Styles>
-    <Container fluid className="contact-container ">
-     
+  function sendEmail(e) {
+    e.preventDefault();
 
-    </Container>
-    </Styles>
+    emailjs.sendForm('service_2na33qf', 'template_7vkoo9v', e.target, 'user_zDDNKotipzbFYBIfAKtC1')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    
+    <Form className="contact-form" onSubmit={sendEmail}>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Name address</Form.Label>
+      <Form.Control type="text" placeholder="Enter email" name="user_name" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Password" name="user_email" />
+    </Form.Group>
+
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Message</Form.Label>
+      <Form.Control type="text" placeholder="Password" name="message" />
+    </Form.Group>
+    <Button  variant="primary" type="submit" value="Send" onClick={event =>  window.location.href='/esent'}>
+      Submit
+    </Button>
+
+    </Form>
   );
 }
-
