a2aa9896f387eb3106d50cbcd5696f7436acf706
kppalmo <kepalmo8@gmail.com>
2021-07-11 14:10:56 -0400

    bugFix

--- src/Pages/MAINPAGE.js
+++ src/Pages/MAINPAGE.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { LandingPage } from './LandingPage';
-import { Footer } from '../utils/Footer';
-import { Navigation } from '../utils/Navigation';
+
 import ContactUs from '../utils/ContactUs';
 
 export const MAINPAGE = () => (
